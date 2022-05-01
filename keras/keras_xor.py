0e349aca2225ee542458cda635c67b955e4b25fd
reapersy <reapersoy@proton.me>
2022-05-01 22:23:07 +0000

    Version 1.1r9

--- keras/keras_xor.py
+++ keras/keras_xor.py
@@ -25,3 +25,39 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 model = Sequential()  # 定义一个序贯模型
+
+# 添加一个全连接层，输入层有2个特征，后面的隐藏层有2个单元
+# 隐藏层的激活函数为tanh
+# 所有层的bias都是自动添加的
+model.add(Dense(2, activation='tanh', input_shape=(2,)))
+
+# 最后的输出也是一个全连接层，激活函数为sigmoid
+model.add(Dense(1, activation='sigmoid'))
+
+# check the structure and parameters of this model
+model.summary()
+
+# 优化器
+sgd = SGD(lr=0.8) # learning rate
+
+# 模型训练(fit)前需要编译
+model.compile(loss='binary_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+# 自动求导，梯度下降和反向传播
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+
+# 利用测试样本评价模型，可以计算loss值以及测试集中的正确率
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# 利用模型进行预测
+print(model.predict_proba(x_train))
+# https://github.com/tensorflow/tensorflow/issues/3388
+K.clear_session()
---
+++ probability_basic/LLN_and_CLT/CLT.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+"""
+Created on Sun Nov 12 08:44:37 2017
+
+@author: Belter
+"""
+
+
+def sampling2pmf(n, dist, m=100000):
+    """
+    n: sample size for each experiment
+    m: how many times do you do experiment, fix in 100000
+    dist: frozen distribution
+    """
+    current_dist = dist
+    sum_of_samples = []
+    for i in range(m):
+        samples = current_dist.rvs(size=n)  # 与每次取一个值，取n次效果相同
+        # print(samples)
+        sum_of_samples.append
