8052c9a284d593e15ae63b8607dd7d76279ab03f
reapersy <reapersoy@proton.me>
2022-05-08 07:06:03 +0000

    Updating bom on ea to 2.21ea-SNAPSHOT

--- probability_basic/LLN_and_CLT/CLT.py
+++ probability_basic/LLN_and_CLT/CLT.py
@@ -21,4 +21,23 @@ def sampling2pmf(n, dist, m=100000):
     for i in range(m):
         samples = current_dist.rvs(size=n)  # 与每次取一个值，取n次效果相同
         # print(samples)
-        sum_of_samples.append
+        sum_of_samples.append(np.sum(samples))
+    # 下面计算频率的方式不适合连续型随机变量，因此直接返回随机变量的和
+    # val, cnt = np.unique(sum_of_samples, return_counts=True)
+    # pmf = cnt / len(sum_of_samples)
+    # return val, pmf
+    return sum_of_samples
+
+
+def plot(n, dist, subplot, plt_handle, dist_type):
+    """
+    :param n: sample size
+    :param dist: distribution of each single sample
+    :param subplot: location of sub-graph, such as 221, 222, 223, 224
+    :param plt_handle: plt object
+    :param dist_type: the type of distribution
+    :return: plt object
+    """
+    bins = 20000
+    plt = plt_handle
+    plt.subplot(sub
