9487599742f2f68fa15b648dc0609cf2b44ef2dc
reapersy <reapersoy@proton.me>
2022-08-22 05:25:38 +0000

    adding travis yml

--- probability_basic/discrete_distributions/draw_pmf.py
+++ probability_basic/discrete_distributions/draw_pmf.py
@@ -50,3 +50,23 @@ def poisson_pmf(mu=3):
     泊松分布
     https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.poisson.html#scipy.stats.poisson
     :param mu: 单位时间（或单位面积）内随机事件的平均发生率
+    :return:
+    """
+    poisson_dis = stats.poisson(mu)
+    x = np.arange(poisson_dis.ppf(0.001), poisson_dis.ppf(0.999))
+    print(x)
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(x, poisson_dis.pmf(x), 'bo', ms=8, label='poisson pmf')
+    ax.vlines(x, 0, poisson_dis.pmf(x), colors='b', lw=5, alpha=0.5)
+    ax.legend(loc='best', frameon=False)
+    plt.ylabel('Probability')
+    plt.title('PMF of poisson distribution(mu={})'.format(mu))
+    plt.show()
+
+# poisson_pmf(mu=8)
+
+
+def plot_bar():
+    y = [1, 2, 3, 4, 5]
+    x_name = ['apple', 'orange', 'pear', 'mango', 'peach']
+ 
