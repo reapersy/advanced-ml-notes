9b994e487cd44e83271b3a839156a77237d5b525
reapersy <reapersoy@proton.me>
2022-10-13 22:00:17 +0000

    fix travis config

--- probability_basic/three_sampling_distributions/chi-square distribution.py
+++ probability_basic/three_sampling_distributions/chi-square distribution.py
@@ -18,4 +18,20 @@ def chi2_distribution(df=1):
     ax.plot(x, stats.chi2.pdf(x, df), 'r-',
             lw=5, alpha=0.6, label=r'$\chi^2$ pdf')
 
-    # 从冻结
+    # 从冻结的均匀分布取值, Freeze the distribution and display the frozen pdf
+    chi2_dis = stats.chi2(df=df)
+    ax.plot(x, chi2_dis.pdf(x), 'k-',
+            lw=2, label='frozen pdf')
+
+    # 计算ppf分别等于0.001, 0.5, 0.999时的x值
+    vals = chi2_dis.ppf([0.001, 0.5, 0.999])
+    print(vals)  # [ 2.004  4.     5.996]
+
+    # Check accuracy of cdf and ppf
+    print(np.allclose([0.001, 0.5, 0.999], chi2_dis.cdf(vals)))  # Ture
+
+    # Generate random numbers
+    r = chi2_dis.rvs(size=10000)
+    ax.hist(r, normed=True, histtype='stepfilled', alpha=0.2)
+    plt.ylabel('Probability')
+    plt.title(r'PDF of $\chi
