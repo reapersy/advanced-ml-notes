590c39d42b3e3dabce5d57b6c59b1c79dbaf394c
reapersy <reapersoy@proton.me>
2022-10-13 20:37:07 +0000

    adding versioning data

--- probability_basic/distributions_derived_from_the_normal_distribution/t_dis.py
+++ probability_basic/distributions_derived_from_the_normal_distribution/t_dis.py
@@ -52,3 +52,12 @@ def calculate_p_value(data, ref_value):
     :param ref_value: 与data均值进行比较的参考值
     :return: p-value
     """
+    t_val = (ref_value - np.mean(data)) / stats.sem(data)
+    t_dist = stats.t(len(data) - 1)
+    return 2*t_dist.sf(t_val)
+# 计算p值，t-test
+# 零假设：学生的平均成绩与110分没有差别
+scores = np.array([109.4, 76.2, 128.7, 93.7, 85.6, 117.7,
+                   117.2, 87.3, 100.3, 55.1])
+print(calculate_p_value(data=scores, ref_value=110))
+# 0.0995384365279, p-value不显著
