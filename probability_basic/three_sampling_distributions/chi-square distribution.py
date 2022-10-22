bd4bca6793a396efe2956d7bf1fc839a7f4c2e05
reapersy <reapersoy@proton.me>
2022-10-22 07:28:59 +0000

    Added header image

--- probability_basic/three_sampling_distributions/chi-square distribution.py
+++ probability_basic/three_sampling_distributions/chi-square distribution.py
@@ -53,3 +53,12 @@ def diff_chi2_dis():
     # x1 = np.linspace(chi2_dis_0_5.ppf(0.01), chi2_dis_0_5.ppf(0.99), 100)
     x2 = np.linspace(chi2_dis_1.ppf(0.65), chi2_dis_1.ppf(0.9999999), 100)
     x3 = np.linspace(chi2_dis_4.ppf(0.000001), chi2_dis_4.ppf(0.999999), 100)
+    x4 = np.linspace(chi2_dis_10.ppf(0.000001), chi2_dis_10.ppf(0.99999), 100)
+    x5 = np.linspace(chi2_dis_20.ppf(0.00000001), chi2_dis_20.ppf(0.9999), 100)
+    fig, ax = plt.subplots(1, 1)
+    # ax.plot(x1, chi2_dis_0_5.pdf(x1), 'b-', lw=2, label=r'df = 0.5')
+    ax.plot(x2, chi2_dis_1.pdf(x2), 'g-', lw=2, label='df = 1')
+    ax.plot(x3, chi2_dis_4.pdf(x3), 'r-', lw=2, label='df = 4')
+    ax.plot(x4, chi2_dis_10.pdf(x4), 'b-', lw=2, label='df = 10')
+    ax.plot(x5, chi2_dis_20.pdf(x5), 'y-', lw=2, label='df = 20')
+    plt
