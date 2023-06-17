69d301fb1ecfbff9c98d82f268f4ee504d0b5eee
reapersy <reapersoy@proton.me>
2023-06-17 20:09:02 +0000

    Fix publish script

--- scikit_learn/classification/binary_classifier.py
+++ scikit_learn/classification/binary_classifier.py
@@ -25,4 +25,18 @@ some_digit = X[36000]
 # show digit's image
 def show_digit_image(digit_features):
     some_digit_image = some_digit.reshape(28, 28)
-   
+    plt.imshow(some_digit_image, cmap=matplotlib.cm.binary, interpolation='nearest')
+    plt.axis('off')
+    plt.show()
+# show_digit_image(some_digit)
+# print(y[36000])
+
+# The MNIST dataset is acturally already split into a training set(the first 60,000 images)
+# and a test set(the last 10,000 images)
+train_num = 60000
+X_train, X_test, y_train, y_test = X[:train_num], X[train_num:], y[:train_num], y[train_num:]
+
+# shuffle the training set, 打乱训练样本的次序，这一步在有些时候挺有用的
+# 比如样本是排过序的，shuffle后可以保证每种数字的分布是均匀的
+shuffle_index = np.random.permutation(60000) # 这个函数不错
+# pri
