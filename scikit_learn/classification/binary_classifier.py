21c1e7f2fbddb737ba226d8a3fce0a347ea39d04
reapersy <reapersoy@proton.me>
2023-06-24 13:45:08 +0000

    Release version 0.5.0

--- scikit_learn/classification/binary_classifier.py
+++ scikit_learn/classification/binary_classifier.py
@@ -39,4 +39,21 @@ X_train, X_test, y_train, y_test = X[:train_num], X[train_num:], y[:train_num], 
 # shuffle the training set, 打乱训练样本的次序，这一步在有些时候挺有用的
 # 比如样本是排过序的，shuffle后可以保证每种数字的分布是均匀的
 shuffle_index = np.random.permutation(60000) # 这个函数不错
-# pri
+# print(y_train)
+# print(shuffle_index)
+X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+## training a binary classifier
+y_train_5 = (y_train == 5)  # True for all 5s, False for all other digits
+y_test_5 = (y_test == 5)
+
+# 建立模型，参数random_state用于shuffle data
+sgd_clf = SGDClassifier(random_state=42)
+# 训练模型，用所有的training data, 但是标签只有True(5)和False(非5)两类
+sgd_clf.fit(X_train, y_train_5)
+# 预测
+predict_result = sgd_clf.predict([some_digit])
+print(predict_result)
+
+## performance measures
+# measuring accuracy using cross-vali
