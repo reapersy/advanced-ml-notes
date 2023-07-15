74cf1fea5eb8f0fbd955112b7d1e062c97a6dd7d
reapersy <reapersoy@proton.me>
2023-07-15 06:25:37 +0000

    minor work

--- scikit_learn/classification/binary_classifier.py
+++ scikit_learn/classification/binary_classifier.py
@@ -140,4 +140,15 @@ from sklearn.ensemble import RandomForestClassifier
 forest_clf = RandomForestClassifier(random_state=42)
 y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3, method='predict_proba')
 # print(y_probas_forest, y_probas_forest.shape)
-# print(X_train[
+# print(X_train[36000])
+# print(y_probas_forest[36000])
+y_scores_forest = y_probas_forest[:, 1]  # score = probability of positive class
+print(y_scores_forest)
+#---- compare ROC
+fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5, y_scores_forest)
+plt.plot(fpr, tpr, 'b:', label='SGD')
+plot_roc_curve(fpr_forest, tpr_forest, 'Random Forest')
+plt.legend(loc='lower right')
+plt.show()
+print('Random forest\'s AUC is', (roc_auc_score(y_train_5, y_scores_forest)))
+
---
+++ scikit_learn/generalized_linear_models.py
@@ -0,0 +1,4 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 19 16:26:08 2017
