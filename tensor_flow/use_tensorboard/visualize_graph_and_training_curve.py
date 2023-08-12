bf8355ac8009e30e862e8e4c7ebbf8257bbed7e9
reapersy <reapersoy@proton.me>
2023-08-12 09:47:53 +0000

    added package-lock.json

--- tensor_flow/use_tensorboard/visualize_graph_and_training_curve.py
+++ tensor_flow/use_tensorboard/visualize_graph_and_training_curve.py
@@ -5,4 +5,19 @@
 import numpy as np
 from sklearn.datasets import fetch_california_housing
 import tensorflow as tf
-from sklearn.preprocessing import StandardScaler  #  用于
+from sklearn.preprocessing import StandardScaler  #  用于数据缩放
+from datetime import datetime
+
+# 按照时间创建存放log文件的文件夹，用于tensorBoard的可视化
+now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+root_logdir = 'tf_logs'
+logdir = '{}/run-{}'.format(root_logdir, now)
+housing = fetch_california_housing()
+m, n = housing.data.shape  # m是样本数，n是特征的数量
+print(m, n)
+
+# Gradient Descent requires scaling the feature vectors first
+# X的缩放对后面的训练过程影响非常大，经过缩放的数据经过很少的迭代次数就可以收敛，学习率可以设得很大
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data_plus_bias = np.c_[np.on
